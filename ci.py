5625c2e9a3d44317f491e4cf32ceb47646f077fb
Diksha <97683112+dikshagairola@users.noreply.github.com>
2025-10-28 23:11:24 +0530

    Delete ci.py

--- ci.py
+++
@@ -1,7 +0,0 @@
-principal= float(input("Enter the principal amount: "))
-rate= float(input("Enter the rate"))
-time= float(input("Enter the time"))
-amount=principal*(1+rate/100)**time
-ci=amount-principal
-print("compound interest is", ci)
-
